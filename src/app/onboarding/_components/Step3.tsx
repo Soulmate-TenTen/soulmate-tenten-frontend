9e5d8eb76cc00f92349e69af4101c8b380b286f5
nno3onn <nno3onn@naver.com>
2025-08-03 21:45:21 +0900

    feat: 온보딩 step 1~5 화면 개발

---
+++ src/app/onboarding/_components/Step3.tsx
@@ -0,0 +1,29 @@
+import { Step3CategoryList, Step3CategoryType } from "@/types/onboarding";
+import StepRadioList from "./StepRadioList";
+import StepLayout from "./StepLayout";
+import { useStepFormStore } from "@/store/useStepFormStore";
+import { useState } from "react";
+
+export default function Step3() {
+  const { setData } = useStepFormStore();
+  const [checked, setChecked] = useState<Step3CategoryType | undefined>(undefined);
+
+  const handleClick = (value: Step3CategoryType) => {
+    setChecked(value);
+    setData("step3", checked as Step3CategoryType);
+  };
+
+  return (
+    <StepLayout
+      text={
+        <>
+          후회할 때 가장 많이
+          <br />
+          하는 생각은 무엇인가요?
+        </>
+      }
+    >
+      <StepRadioList direction="column" list={Step3CategoryList} onClick={handleClick} checked={checked} />
+    </StepLayout>
+  );
+}
