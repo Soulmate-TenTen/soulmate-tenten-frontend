141beb9db60b9a4712c333df90c74f834b416527
chucoding <chucoding@gmail.com>
2025-08-04 01:27:22 +0900

    feat : login page title & character(temporary)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,8 @@ import { signIn } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
 import Image from "next/image";
 import kakaoIcon from "../../assets/images/kakao-icon.png";
+import title from "../../assets/images/title.svg";
+import character from "../../assets/images/character.svg";
 
 export default function LoginPage() {
   const searchParams = useSearchParams();
@@ -11,25 +13,17 @@ export default function LoginPage() {
   
   return (
     <div className="relative min-h-screen bg-[#000414] flex flex-col items-center justify-center overflow-hidden">
-      {/* Main Content */}
       <div className="flex flex-col items-center justify-center flex-1 px-8">
-        {/* Logo Area */}
-        <div className="text-center mb-12">
-          {/* Logo Text */}
-          <div className="mb-8">
-            <div className="text-[#FFFFF6] font-bold text-3xl mb-1 font-pretendard">SOUL MATE</div>
-          </div>
+        <div className="mb-12">
+          <Image src={title} alt="소울메이트 로고" />
         </div>
-
-        {/* Tagline */}
         <div className="text-center mb-16 px-4">
           <p className="text-[#FFFFF6] text-base leading-relaxed font-suit font-bold">
             후회를 줄이고 싶은 사람들을 위한,<br />
             결정의 순간을 함께하는 단 하나의 파트너
           </p>
         </div>
-
-        {/* Kakao Login Button */}
+        <Image src={character} alt="소울메이트 캐릭터" />
         <div className="relative z-10">
           <button 
             onClick={() => signIn("kakao", { callbackUrl: callbackUrl || "/" })}
@@ -43,7 +37,7 @@ export default function LoginPage() {
                 height={16}
                 className="w-4 h-4"
               />
-              <span className="text-base font-extrabold font-pretendard">카카오로 시작하기</span>
+              <span className="text-base font-extrabold font-pretendard cursor-pointer">카카오로 시작하기</span>
             </div>
           </button>
         </div>
