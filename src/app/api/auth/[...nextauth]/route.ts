644fddaaf60ea3841efda3aec56d260ac9f06b2c
chucoding <chucoding@gmail.com>
2025-08-04 23:00:15 +0900

    feat : 로그인 후 backend auth API 호출

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
-import NextAuth from "next-auth";
+import NextAuth, { Session, User } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import Kakao from "next-auth/providers/kakao";
+import { getSession } from "next-auth/react";
 
 const authOptions = {
   providers: [
@@ -11,6 +13,47 @@ const authOptions = {
   pages: {
     signIn: "/login",
     signOut: "/login"
+  },
+  callbacks: {
+    async signIn({ user }: { user: User }) {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: user.name
+        })
+      });
+      if (response.ok) {
+        const data = await response.json();
+        user.newMemberYn = data.newMemberYn;
+      }
+      return true;
+    },
+    async redirect({ url, baseUrl }: { url: string, baseUrl: string }) {
+      if (url.startsWith(baseUrl)) {
+        const session = await getSession();
+        if (session?.user?.newMemberYn === 'Y') {
+          return `${baseUrl}/onboarding`;
+        } else {
+          return `${baseUrl}/`;
+        }
+      }
+      return url;
+    },
+    async session({ session, token }: { session: Session, token: JWT }) {
+      if (token.newMemberYn) {
+        session.user.newMemberYn = token.newMemberYn;
+      }
+      return session;
+    },
+    async jwt({ token, user }: { token: JWT, user: User }) {
+      if (user?.newMemberYn) {
+        token.newMemberYn = user.newMemberYn;
+      }
+      return token;
+    }
   },
 };
 
