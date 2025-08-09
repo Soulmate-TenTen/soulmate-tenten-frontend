71537659273a9bc79e94ecb3fcee49504798a233
nno3onn <nno3onn@naver.com>
2025-08-10 00:44:00 +0900

    fix: progress bar rounded

--- src/app/onboarding/_components/StepLayout.tsx
+++ src/app/onboarding/_components/StepLayout.tsx
@@ -21,8 +21,8 @@ export default function StepLayout({ children, text }: IStepLayout) {
   }, [step]);
 
   const handleBackClick = () => {
-    const fromMypage = searchParams.get('from') === 'mypage';
-    
+    const fromMypage = searchParams.get("from") === "mypage";
+
     if (step === 1 && fromMypage) {
       router.push("/mypage");
     } else {
@@ -33,17 +33,10 @@ export default function StepLayout({ children, text }: IStepLayout) {
   return (
     <div className="mt-2">
       {/* 뒤로 가기 버튼 */}
-      <Image 
-        className="cursor-pointer" 
-        src="/back-icon.svg" 
-        width={32} 
-        height={32} 
-        alt="뒤로" 
-        onClick={handleBackClick} 
-      />
+      <Image className="cursor-pointer" src="/back-icon.svg" width={32} height={32} alt="뒤로" onClick={handleBackClick} />
 
       {/* progress bar */}
-      <div className="mt-5 w-full bg-[#444] rounded-full h-2 mx-2 mb-[15%]">
+      <div className="mt-5 bg-[#444] rounded-full h-2 mx-2 mb-[15%]">
         <div className="bg-[#fffac4] h-full rounded-full transition-[width] duration-200 ease-in-out" style={{ width: `${progress}%` }} />
       </div>
 
