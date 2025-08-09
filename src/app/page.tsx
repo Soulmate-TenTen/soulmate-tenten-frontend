a3799efe96d2535aa96a26e3216919b0dd48cdde
nno3onn <nno3onn@naver.com>
2025-08-10 00:37:34 +0900

    fix: 홈에서 텍스트 font-weight 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default function HomePage() {
         <div className="flex justify-between gap-5 p-4 mx-auto bg-[#27272A] rounded-xl cursor-pointer" onClick={goChat}>
           <Image src="/star.svg" width={50} height={50} alt="별 캐릭터" />
           <div>
-            <p>오늘은 어떤 선택이 있으신가요?</p>
+            <p className="font-bold">오늘은 어떤 선택이 있으신가요?</p>
             <p className="text-[12px] mt-1">틀린 선택은 없어요. 저와 함께 이야기해봐요.</p>
           </div>
           <Image src="/right-arrow.svg" width={20} height={20} alt="오른쪽 화살표" />
