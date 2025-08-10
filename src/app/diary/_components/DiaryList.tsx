21c406cc70f53ec01663fe75e24c4c8b293a5e7a
nno3onn <nno3onn@naver.com>
2025-08-10 13:44:52 +0900

    chore: 불필요한 코드 제거

--- src/app/diary/_components/DiaryList.tsx
+++ src/app/diary/_components/DiaryList.tsx
@@ -42,20 +42,6 @@ export default function DiaryList({ diaryList, selectedDateText }: IDiaryList) {
               </div>
             </div>
           ))}
-          {diaryList.map((diary) => (
-            <div key={diary.id} className="text-black p-4 rounded-xl bg-white flex gap-1 cursor-pointer" onClick={() => onClick(diary.id)}>
-              <div className="grow flex flex-col gap-1 min-w-0">
-                <div className="text-[14px]">{diary.title}</div>
-                <div className="text-[12px] line-clamp-2 break-words">{diary.summary}</div>
-              </div>
-              <div
-                style={{ color: ROAD_STATUS_COLOR[diary.roadStatus] }}
-                className={`rounded-full whitespace-nowrap px-2 h-4 text-[10px] ${diary.roadStatus === "회고완료" && "bg-[#EAFFE9]"}`}
-              >
-                {diary.roadStatus}
-              </div>
-            </div>
-          ))}
         </div>
       </div>
     </section>
