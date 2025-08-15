229b8cadcb00660b1e867ceb53b4fe83799d30ee
chucoding <chucoding@gmail.com>
2025-08-16 00:48:27 +0900

    feat : 스트리밍 애니메이션 추가

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,16 +1,46 @@
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import 'github-markdown-css/github-markdown.css';
+import { useEffect, useRef } from 'react';
+import { useChatStore } from '@/store/useChatStore';
 
 interface MarkdownProps {
   text: string;
 }
 
 const Markdown: React.FC<MarkdownProps> = ({ text }) => {
+  const markdownRef = useRef<HTMLDivElement>(null);
+  const { roadId } = useChatStore(); //TODO Refactor : remove dependency on roadId
+
+  useEffect(() => {
+    if (markdownRef.current && roadId === 0) {
+      const elements = markdownRef.current.querySelectorAll('*');
+      elements.forEach((element, index) => {
+        if (element instanceof HTMLElement) {
+          element.style.opacity = '0';
+          element.style.animation = `fadeIn 1.2s ease-in-out ${index * 0.1}s forwards`;
+        }
+      });
+    }
+  }, [text]);
+
   return (
-    <ReactMarkdown remarkPlugins={[remarkGfm]}>
-      {text}
-    </ReactMarkdown>
+    <div ref={markdownRef}>
+      <style jsx>{`
+        @keyframes fadeIn {
+          from {
+            opacity: 0;
+          }
+          to {
+            opacity: 1;
+          }
+        }
+
+      `}</style>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>
+        {text}
+      </ReactMarkdown>
+    </div>
   );
 };
 
